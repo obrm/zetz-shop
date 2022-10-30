3901e87eb70f8832ce14c10796178680f434e21e
Ori Baram <obrm770@gmail.com>
2022-10-30 11:32:00 +0200

    remove react-google-login

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Form, Button, Row, Col } from 'react-bootstrap'
-import { GoogleLogin } from 'react-google-login'
 import { useDispatch, useSelector } from 'react-redux'
 import { Helmet } from 'react-helmet'
 import Message from '../components/Message'
